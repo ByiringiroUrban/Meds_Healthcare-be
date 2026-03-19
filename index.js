ebd34fa0c13d4b959099a85f753c23f9396a4995
ByiringiroUrban <byiringirourban20@gmail.com>
2026-03-19 12:08:40 +0200

    updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"]
   }
 });
